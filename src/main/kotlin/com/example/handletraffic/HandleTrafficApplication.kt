c30029ec2ebb35ed01cf38d9e4df25015efac5dd
sang5c <skah321@gmail.com>
2023-08-10 00:35:01 +0900

    init

---
+++ src/main/kotlin/com/example/handletraffic/HandleTrafficApplication.kt
@@ -0,0 +1,11 @@
+package com.example.handletraffic
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HandleTrafficApplication
+
+fun main(args: Array<String>) {
+    runApplication<HandleTrafficApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/handletraffic/HandleTrafficApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.handletraffic
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HandleTrafficApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
